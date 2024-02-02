0617b202a5eb3e71299af76883958fcce7c91600
Merlin 2100 <merlin2100nft@gmail.com>
2024-02-02 17:09:43 +0100

    Solidity version problem

--- test/MagicContract.t.sol
+++ test/MagicContract.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.19;
 
 import {Test, StdAssertions, Vm} from "../lib/forge-std/src/Test.sol";
 import {MagicContract} from "../src/MagicContract.sol";
