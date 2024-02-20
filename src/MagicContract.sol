6a11ebe5ecf686ee2a5177a2c766ac6d1eb26e6a
Merlin2100 <merlin2100nft@gmail.com>
2024-02-20 16:00:58 +0100

    Solidity Style

--- src/MagicContract.sol
+++ src/MagicContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 /// @title MagicContract: A basic ERC-20 token contract
 /// @author https://github.com/Merlin2100
